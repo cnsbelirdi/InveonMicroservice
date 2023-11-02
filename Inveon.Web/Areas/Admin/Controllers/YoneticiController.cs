926881d0c844058eb582e1c625676c22568ac4b3
Cansu Belirdi <cnsbelirdi@gmail.com>
2023-11-02 16:42:45 +0300

    Add project files.

---
+++ Inveon.Web/Areas/Admin/Controllers/YoneticiController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Inveon.Web.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	public class YoneticiController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+		public IActionResult Git()
+		{
+			return View();
+		}
+		public IActionResult AdminLogout()
+		{
+			return SignOut(new AuthenticationProperties
+			{
+				RedirectUri = "/" // Kullanıcı çıkış yaptığında nereye yönlendirileceğini belirtin
+			}, "Cookies", "oidc");
+		}
+	}
+}
